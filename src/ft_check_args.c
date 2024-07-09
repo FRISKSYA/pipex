591b8d9fafa0c50f3aa406ccc7ab9159a3f17b0d
kfukuhar <kfukuhar@student.42tokyo.jp>
2024-07-09 21:33:35 +0900

    wip

--- src/ft_check_args.c
+++ src/ft_check_args.c
@@ -6,26 +6,38 @@
 /*   By: kfukuhar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 19:00:00 by kfukuhar          #+#    #+#             */
-/*   Updated: 2024/07/08 19:42:24 by kfukuhar         ###   ########.fr       */
+/*   Updated: 2024/07/09 20:53:29 by kfukuhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "../include/pipex.h"
+#include "../include/pipex.h"
 
-//static int	check_cmd(char *cmd);
-
-int	ft_check_args(int argc, char **argv)
+// FIXME: probably dont have to exit in here.
+int	ft_check_args(int argc, char **argv, t_pipex *data)
 {
-	int	i;
+	int		i;
+	int		flags;
+	mode_t	mode;
 
 	if (argc != 5)
 		return (ARGS_ERROR);
 	i = 0;
 	if (access(argv[++i], F_OK | R_OK))
-		ft_exit("INFILE_ACCESS_ERROR");
-//	if (check_cmd(argv[++i])
-//		ft_exit("CMD_ERROR");
-	while (i++ < argc - 1)
-		ft_printf("%s\n", argv[i]);
+		data->is_invalid_infile = true;//ft_exit("INFILE_ACCESS_ERROR");
+	else
+		data->in_fd = open(argv[i], O_RDONLY);
+	if (data->in_fd < 0)
+		ft_exit("INFILE_OPEN_ERROR");
+	while (i++ < argc - 2) ft_printf("%s\n", argv[i]);
+	flags = O_WRONLY | O_CREAT | O_TRUNC;
+	mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
+	if (!access(argv[i], F_OK | R_OK))
+	{
+		data->out_fd = open(argv[i], flags, mode);
+		if (!data->out_fd)
+			ft_exit("OUTFILE_OPEN_ERROR");
+	}
+	else
+		ft_exit("OUTFILE_ACCESS_ERROR");
 	return (SUCCESS);
 }
--- src/pipex.c
+++ src/pipex.c
@@ -6,21 +6,29 @@
 /*   By: kfukuhar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/26 17:00:52 by kfukuhar          #+#    #+#             */
-/*   Updated: 2024/07/08 19:16:54 by kfukuhar         ###   ########.fr       */
+/*   Updated: 2024/07/09 20:46:11 by kfukuhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/pipex.h"
 
-int	main(int argc, char **argv)
+int	main(int argc, char **argv, const char ** const env)
 {
 	t_pipex	*data;
+	int		i;
 
+	i = 0;
+	while (env[i])
+	{
+		if (ft_strnstr(env[i], "PATH", 4))
+			ft_printf("env[%d] : %s\n", i, env[i]);
+		i++;
+	}
 	data = (t_pipex *)malloc(sizeof(t_pipex));
 	if (data == NULL)
 		ft_exit("MALLOC_ERROR");
 	ft_init_pipex(data);
-	if (ft_check_args(argc, argv) == ARGS_ERROR)
+	if (ft_check_args(argc, argv, data) == ARGS_ERROR)
 		ft_exit("ARGS_ERROR");
 	// ft_parse_cmds();
 	// ft_parse_args();
