51d1cd077ba88f080ad29c437e22c364cd95fdbc
kfukuhar <kfukuhar@student.42tokyo.jp>
2024-08-10 19:51:59 +0900

    fixed perror

--- src/process_exc_cmd.c
+++ src/process_exc_cmd.c
@@ -6,7 +6,7 @@
 /*   By: kfukuhar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/10 14:42:45 by kfukuhar          #+#    #+#             */
-/*   Updated: 2024/08/10 18:32:23 by kfukuhar         ###   ########.fr       */
+/*   Updated: 2024/08/10 19:51:32 by kfukuhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,19 @@
 void	execve_relative_path(char **cmd_args, char **env)
 {
 	if (access(cmd_args[0], X_OK) != 0)
+	{
+		perror(cmd_args[0]);
 		return ;
+	}
 	execve(cmd_args[0], cmd_args, env);
 }
 
 void	execve_full_path(char **cmd_args, char **env)
 {
 	if (access(cmd_args[0], X_OK) != 0)
+	{
+		perror(cmd_args[0]);
 		return ;
+	}
 	execve(cmd_args[0], cmd_args, env);
 }
